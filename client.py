230ec9367366d0d280a452b15168adfd8463c642
NejcBoltez <nejc.boltez@gmail.com>
2023-10-15 12:15:00 +0200

    Added speach functionality

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 import requests
 import json
 from os import system
+import webbrowser
+import pyttsx3 as pyttsx
 
-resPost = requests.post('http://192.168.64.119:1234/sendCommand', params = {'command' : 'Whos is BIll Gates'})
+def speak():
+    speech_engine = pyttsx.init()
+    besedilo="How are you"
+    speech_engine.say(besedilo)
+    speech_engine.runAndWait()
+
+webbrowser.open_new('http://192.168.64.55:8000/dashboard')
+
+resPost = requests.post('http://192.168.64.55:1234/sendCommand', params = {'command' : 'Whos is BIll Gates'})
 print (json.loads(resPost.content.decode())['answer'])
 
-system("say "+json.loads(resPost.content.decode())['answer'])
+speak(json.loads(resPost.content.decode())['answer'])
+#system("say "+json.loads(resPost.content.decode())['answer'])
